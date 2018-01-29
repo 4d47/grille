cd34a882dcfcca45098b58d7bddbb21e141aa7c4
Mathieu Gagnon <mathieu@gagnon.name>
2018-01-29 16:57:58 -0500

    changes

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
+//
+// - onclick selection le nombre (pour corriger plus facilement)
+// - dans les moins ne pas aller sous zero
+// - remember from value to reload (always work on addition)
+// - mettre chrono automatiquement sous le score (en petit)
 
 var
+    form = document.querySelector('form'),
     timer = document.querySelector('#timer'),
     secs = document.querySelector('#secs'),
     score = document.querySelector('#score'),
+    stickers = ['😏', '😎', '😉', '😊', '😋', '😺', '★', '✿'],
+    stickersColors = ['#FF851B', '#0074D9', '#FF4136', '#F012BE',
+                      '#B10DC9', '#2ECC40', '#3D9970', '#FFDC00'],
     chronoId,
     chronoSecs;
+
 
 
 // parse a `s` comma separated list of numbers
 // and ranges to an array of integers.
 function parseNumbers(s) {
-    xs = s.split(',').map(function(x) {
+    result = [];
+    s.split(',').forEach(function(x) {
         if (m = x.match(/(\d+)\-(\d+)/)) {
-            var r = [];
             for (i = parseInt(m[1]); i <= m[2]; i++) {
-                r.push(i);
+                result.push(i);
             }
-            return r;
         } else {
-            return parseInt(x);
+            result.push(parseInt(x));
         }
     });
-    return _.uniq(_.flatten(xs));
+    return result.filter(function(v, i, s) { return s.indexOf(v) === i; });
 }
 
 // perform elementry arithmetic
@@ -37,22 +46,32 @@ function calc(a, o, b) {
 function correct() {
     var
         i,
+        good,
         ops = document.querySelectorAll('.op');
 
     for (i = 0; i < ops.length; i++) {
         var
-        a = parseInt(ops[i].querySelector('.a').innerHTML),
+            a = parseInt(ops[i].querySelector('.a').innerHTML),
             o = ops[i].querySelector('.o').innerHTML,
             b = parseInt(ops[i].querySelector('.b').innerHTML),
             ans = parseInt(ops[i].querySelector('input').value);
-        ops[i].classList.toggle('err', calc(a, o, b) != ans);
+        if (calc(a, o, b) != ans) {
+            ops[i].classList.add('err');
+            ops[i].querySelector('input').value = '';
+        } else {
+            ops[i].classList.remove('err');
+        }
     }
 
-    score.innerHTML = (ops.length - document.querySelectorAll('.err').length) + '/' + ops.length;
+    good = ops.length - document.querySelectorAll('.err').length;
+    score.innerHTML = good + '/' + ops.length;
+    if (good === ops.length) {
+        score.innerHTML += '<span style="font-size:1.8em;vertical-align:text-top;color:' + pick(stickersColors) + '">' + pick(stickers) + '</span>';
+    }
 
 }
 
-// give uneasy number four chances to sample
+// weight out easy numbers so they dont appear so often
 function weight(xs) {
     return xs.reduce(function (acc, value, index, array) {
         return acc.concat([0,1,10].includes(value) ? [value] : [value, value, value, value]);
@@ -64,13 +83,13 @@ function redraw() {
     var
         i, node,
         main = document.querySelector('main'),
+        footer = document.querySelector('footer'),
         o = document.querySelector('#o').value,
-        q = document.querySelector('#q').value,
-        n = document.querySelector('#n').value,
-        xs = parseNumbers(n),
-        ten = weight(parseNumbers('0-10')),
+        q = parseInt(document.querySelector('#q').value),
+        xs = parseNumbers(document.querySelector('#n').value),
+        ten = weight(parseNumbers(o == '÷' ? '0-12' : '0-10')),
         ab,
-        end = document.querySelector('.end'),
+        prev = '',
         ops = document.querySelectorAll('.op'),
         op = ops[0];
 
@@ -80,16 +99,43 @@ function redraw() {
         main.removeChild(ops[i]);
     }
     for (i = 0; i < q; i++) {
-        node = op.cloneNode(true),
-            node.querySelector('.o').innerHTML = o;
-        // Add weight to lower chances of 0,1,2,10
-        ab = _.shuffle([_.sample(xs), _.sample(ten)]);
+        do {
+            ab =  [pick(ten), pick(xs)];
+        } while (ab.toString() == prev.toString());
+        prev = ab.toString();
+        if (o == '÷') {
+            ab[0] *= ab[1];
+        } else {
+            ab = shuffle(ab);
+        }
+        node = op.cloneNode(true);
+        node.querySelector('.o').innerHTML = o;
         node.querySelector('.a').innerHTML = ab[0];
         node.querySelector('.b').innerHTML = ab[1];
         node.querySelector('input').value = '';
         node.classList.remove('err');
-        main.insertBefore(node, end);
+        main.insertBefore(node, footer);
     }
+}
+
+// pick a random value out of an array
+function pick(a) {
+    return a[Math.floor(Math.random() * a.length)];
+}
+
+// randomize array element
+function shuffle(a) {
+  var currentIndex = a.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = a[currentIndex];
+    a[currentIndex] = a[randomIndex];
+    a[randomIndex] = temporaryValue;
+  }
+
+  return a;
 }
 
 // format `secs` to mm:ss
@@ -111,20 +157,32 @@ function chronoStart() {
     }, 1000);
 }
 
-// toggle chronometer state
-function chronoClick() {
+function onChronoClick() {
     chronoId ? chronoStop() : chronoStart();
 }
 
-function endClick() {
+function onCompleteClick() {
     chronoStop();
     correct();
 }
 
+function onPrint(e) {
+    e.preventDefault();
+    window.print();
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+    if (!form.checkValidity()) {
+        return;
+    }
+    chronoStop();
+    redraw();
+}
 
 // initialize event handlers
-document.querySelector('#timer button').addEventListener('click', chronoClick); 
-document.querySelector('#ctrl button').addEventListener('click', redraw); 
-document.querySelector('.end button').addEventListener('click', endClick); 
-document.onload = redraw;
+form.addEventListener('submit', onSubmit);
+document.querySelector('#timer button').addEventListener('click', onChronoClick); 
+document.querySelector('form .print').addEventListener('click', onPrint);
+document.querySelector('footer button').addEventListener('click', onCompleteClick); 
 document.addEventListener('DOMContentLoaded', redraw);
