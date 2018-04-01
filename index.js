708b17f7d8ee92e7eb26911c0f1872cccb510aee
Mathieu Gagnon <mathieu@gagnon.name>
2018-03-31 21:23:55 -0400

    Big refactoring inspired by stimulus
    - Bind event handlers at HTML level
    - index.js only define functions
    - Favor named HTML elements to JavaScript selectors
    - Put main in a form so François can press enter at the end

--- index.js
+++ index.js
@@ -1,53 +1,118 @@
+/*
+ * This script only defines functions.
+ *
+ * First the event handlers (using the globals),
+ * then the primary operations (manipulating the DOM)
+ * and finally the utilities (purish functions).
+ *
+ * # Concepts:
+ *
+ * - Readable document
+ * - Persisting state in the DOM
+ * - Avoid selectors when possible and favor named HTML elements
+ *
+ * see: [Stimulus](https://stimulusjs.org)
+ *
+ * # TOC
+ *
+ * Event handlers
+ *   onChangeStopLastUncheck
+ *   onCtrlSubmit
+ *   onPrintClick
+ *   onChronoClick
+ *   onCompleteClick
+ *
+ * Operations
+ *   redraw
+ *   correct
+ *   chronoStop
+ *   chronoStart
+ *
+ * Utilities
+ *   parseNumbers
+ *   indexAt
+ *   calc
+ *   weight
+ *   pick
+ *   shuffle
+ *   mmss
+ */
 
-var
-    main = document.querySelector('main'),
-    footer = document.querySelector('footer'),
-    timer = document.querySelector('#timer'),
-    secs = document.querySelector('#secs'),
-    score = document.querySelector('#score');
+
+/*
+ * Event handlers
+ */
+
+// Reset target checked when checked count goes to zero
+function onChangeStopLastUncheck(e) {
+    if (e.target.form.querySelectorAll('[name="' + e.target.name + '"]:checked').length == 0) {
+        e.target.checked = true;
+    }
+}
+
+function onCtrlSubmit(e) {
+    e.preventDefault();
+    if (!document.ctrl.checkValidity()) {
+        return;
+    }
+    chronoStop(document.ctrl.lastElementChild);
+    redraw(document.ctrl, document.grid);
+}
+
+function onPrintClick(e) {
+    e.preventDefault();
+    window.print();
+}
+
+function onChronoClick(e) {
+    var timer = document.ctrl.lastElementChild;
+    e.preventDefault();
+    if (timer.dataset.chronoId) {
+        chronoStop(timer);
+    } else {
+        chronoStart(timer, document.ctrl.secs);
+    }
+}
+
+function onCompleteClick(e) {
+    e.preventDefault();
+    chronoStop(document.ctrl.lastElementChild);
+    correct(document.grid);
+}
 
 
-// initialize event handlers
-timer.querySelector('button').addEventListener('click', onChronoClick); 
-footer.querySelector('button').addEventListener('click', onCompleteClick); 
-document.addEventListener('DOMContentLoaded', redraw);
-document.form.addEventListener('submit', onFormSubmit);
-document.form.querySelector('.print').addEventListener('click', onPrintClick);
-document.form.opr.forEach(function(el) {
-    // prevent last checkbox to be unchecked
-    el.addEventListener('change', function(e) {
-        if (document.form.querySelectorAll(':checked').length === 0) {
-            el.checked = true;
-        }
-    });
-});
+/*
+ * Operations
+ */
 
-// redraw operation nodes based on user form input
-function redraw() {
+// Redraw grid operation nodes based on user ctrl input
+function redraw(ctrl, grid) {
     var
-        qty = parseInt(document.form.elements.qty.value),
-        nos = parseNumbers(document.form.elements.nos.value),
+        qty = parseInt(ctrl.elements.qty.value),
+        nos = parseNumbers(ctrl.elements.nos.value),
         xs  = weight(parseNumbers('0-10')),
         i,
         node,
         ab,
         prevAb = '',
         opr,
-        ops = main.querySelectorAll('.op'),
+        ops = grid.querySelectorAll('.op'),
         op = ops[0];
 
     op.style.opacity = 0;
-    score.innerHTML = '';
-    main.dataset.sticker = pick(main.dataset.stickers.split(','));
-    ops.forEach(main.removeChild.bind(main));
+    grid.score.innerHTML = '';
+    grid.dataset.currentSticker = pick(grid.dataset.stickers.split(' '));
+    ops.forEach(grid.removeChild.bind(grid));
 
     for (i = 0; i < qty; i++) {
+        // pick two numbers, different from previous selection
         do {
             ab =  [pick(xs), pick(nos)];
         } while (ab.toString() === prevAb.toString());
         prevAb = ab.toString();
-        opr = pick(document.form.querySelectorAll(':checked')).value;
-        // opr = pick(oprSelector.selected).value;
+        // pick a random
+        opr = pick(ctrl.querySelectorAll('[name="opr"]:checked')).value;
+        // adjust ab based on operation
         if (opr === '÷') {
             // make `xs` the answer
             ab[0] *= ab[1];
@@ -57,7 +122,7 @@ function redraw() {
                 return b - a;
             });
         } else {
-            ab = shuffle(ab);
+            shuffle(ab);
         }
         node = op.cloneNode(true);
         node.querySelector('.o').innerHTML = opr;
@@ -65,7 +130,7 @@ function redraw() {
         node.querySelector('.b').innerHTML = ab[1];
         node.querySelector('input').value = '';
         node.classList.remove('err');
-        main.insertBefore(node, footer);
+        grid.insertBefore(node, grid.lastElementChild);
         (function(node) {
             // fade in tumble effect
             setTimeout(function() {
@@ -75,10 +140,10 @@ function redraw() {
     }
 }
 
-// mark invalid operations and show score
-function correct() {
+// Mark invalid operations and show score
+function correct(grid) {
     var
-        ops = main.querySelectorAll('.op'),
+        ops = grid.querySelectorAll('.op'),
         input,
         points = 0;
 
@@ -96,16 +161,40 @@ function correct() {
             op.classList.remove('err');
         }
     });
-    if (input = main.querySelector('.op.err input')) {
+    if (input = grid.querySelector('.op.err input')) {
         input.focus();
     }
-    score.innerHTML = points + '/' + ops.length;
+    grid.score.innerHTML = points + '/' + ops.length;
     if (points === ops.length) {
-        score.innerHTML += '<br><span class="emojis emojis--' + main.dataset.sticker + '"></span>';
+        grid.score.innerHTML += '<br><span class="emojis emojis--' + grid.dataset.currentSticker + '"></span>';
     }
 }
 
-// parse a `s` comma separated list of numbers
+// Puts the timer in stop state
+function chronoStop(timer) {
+    timer.className = 'play';
+    clearInterval(timer.dataset.chronoId);
+    delete timer.dataset.chronoId;
+}
+
+// Puts the timer in start state
+function chronoStart(timer, secs) {
+    var
+        chronoSecs = 0,
+        tick = function() {
+            secs.innerHTML = mmss(chronoSecs++);
+        };
+    tick();
+    timer.className = 'stop';
+    timer.dataset.chronoId = setInterval(tick, 1000);
+}
+
+
+/*
+ * Utilities
+ */
+
+// Parse a `s` comma separated list of numbers
 // and ranges to an array of integers.
 function parseNumbers(s) {
     result = [];
@@ -121,12 +210,12 @@ function parseNumbers(s) {
     return result.filter(indexAt);
 }
 
-// test that the first `v` in `s` is at index `i`
+// Test that the first `v` in `s` is at index `i`
 function indexAt(v, i, s) {
     return s.indexOf(v) === i;
 }
 
-// perform elementry arithmetic
+// Perform elementry arithmetic
 function calc(a, opr, b) {
     switch (opr) {
         case '×': return a * b;
@@ -137,19 +226,19 @@ function calc(a, opr, b) {
     }
 }
 
-// weight out easy numbers so they dont appear so often
+// Weight out easy numbers so they dont appear so often
 function weight(nos) {
     return nos.reduce(function (acc, value, index, array) {
         return acc.concat([0,1,10].includes(value) ? [value] : [value, value, value, value]);
     }, []);
 }
 
-// pick a random value out of an array
+// Pick a random value out of an array
 function pick(a) {
     return a[Math.floor(Math.random() * a.length)];
 }
 
-// randomize array element
+// Randomize array element in place
 function shuffle(a) {
   var currentIndex = a.length, temporaryValue, randomIndex;
 
@@ -160,53 +249,9 @@ function shuffle(a) {
     a[currentIndex] = a[randomIndex];
     a[randomIndex] = temporaryValue;
   }
-
-  return a;
 }
 
-// format `secs` to mm:ss
+// Format `secs` to mm:ss
 function mmss(secs) {
     return new Date(1000 * secs).toISOString().substr(14, 5);
-}
-
-// puts the chrono widget in stop state
-function chronoStop() {
-    timer.className = 'play';
-    clearInterval(timer.dataset.chronoId);
-    delete timer.dataset.chronoId;
-}
-
-// puts the chrono widget in start state
-function chronoStart() {
-    var
-      chronoSecs = 0,
-      tick = function() {
-        secs.innerHTML = mmss(chronoSecs++);
-      };
-    tick();
-    timer.className = 'stop';
-    timer.dataset.chronoId = setInterval(tick, 1000);
-}
-
-function onChronoClick() {
-    timer.dataset.chronoId ? chronoStop() : chronoStart();
-}
-
-function onCompleteClick() {
-    chronoStop();
-    correct();
-}
-
-function onPrintClick(e) {
-    e.preventDefault();
-    window.print();
-}
-
-function onFormSubmit(e) {
-    e.preventDefault();
-    if (!document.form.checkValidity()) {
-        return;
-    }
-    chronoStop();
-    redraw();
 }
--- polyfill.js
+++ polyfill.js
@@ -60,3 +60,25 @@ if (!Array.prototype.includes) {
     }
   });
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/lastElementChild
+// Overwrites native 'lastElementChild' prototype.
+// Adds Document & DocumentFragment support for IE9 & Safari.
+// Returns array instead of HTMLCollection.
+;(function(constructor) {
+    if (constructor &&
+        constructor.prototype &&
+        constructor.prototype.lastElementChild == null) {
+        Object.defineProperty(constructor.prototype, 'lastElementChild', {
+            get: function() {
+                var node, nodes = this.childNodes, i = nodes.length - 1;
+                while (node = nodes[i--]) {
+                    if (node.nodeType === 1) {
+                        return node;
+                    }
+                }
+                return null;
+            }
+        });
+    }
+})(window.Node || window.Element);
