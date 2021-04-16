7a68ec39375d0d4aea81d7d0d7f137927a78e90f
Mathieu Gagnon <mathieu@gagnon.name>
2021-04-16 11:26:46 -0400

    Update to ES2019

--- index.js
+++ index.js
@@ -1,95 +1,78 @@
 /*
- * This script only defines functions.
+ * This script is written in plain (and hopefully simple) JavaScript.
+ * The guiding principles are:
  *
- * First the event handlers (using the globals),
- * then the primary operations (manipulating the DOM)
- * and finally the utilities (purish functions).
+ * - JavaScript only contains function definitions
+ * - HTML declares data and bind the events
  *
- * # Concepts:
+ * This makes:
  *
- * - Readable document
- * - Persisting state in the DOM
- * - Avoid selectors when possible and favor named HTML elements
+ * - The JavaScript file side-effect free.
+ *   eg. does not use global data.
+ * - The HTML more readable, just looking at the 
+ *   source gives you a lot about whats going on.
  *
- * see: [Stimulus](https://stimulusjs.org)
+ * The functions are grouped top down:
  *
- * # TOC
- *
- * Event handlers
- *   onChangeStopLastUncheck
- *   onCtrlSubmit
- *   onAnsKeyUp
- *   onCompleteClick
- *
- * Operations
- *   redraw
- *   correct
- *
- * Utilities
- *   parseNumbers
- *   indexAt
- *   calc
- *   weight
- *   pick
- *   shuffle
- *   mmss
+ * 1. Event handlers
+ * 2. DOM manipulating
+ * 3. Purish functions
  */
 
 
 /*
- * Event handlers
+ * 1. Event handlers
  */
 
 function onChangeStopLastUncheck(e) {
-    if (e.target.form.querySelectorAll('[name="' + e.target.name + '"]:checked').length == 0) {
+    if (!e.target.form.querySelectorAll('[name="' + e.target.name + '"]:checked').length) {
         e.target.checked = true;
     }
 }
 
-function onCtrlSubmit(e) {
+function onSubmit(e) {
     e.preventDefault();
-    if (!document.ctrl.checkValidity()) {
+    if (!e.target.checkValidity()) {
         return;
     }
-    document.grid.reset();
-    redraw(document.ctrl, document.grid);
+    redraw(e.target, document.grid);
     document.grid.ans[0].focus();
-}
-
-function onGridReset(e) {
-    clearInterval(document.grid.dataset.chronoId);
-    delete document.grid.dataset.chronoId;
 }
 
 function onAnsKeyUp(e) {
     var
-      disabled = false,
-      len = 0;
-    document.grid.ans.forEach(function(n) {
-        len += n.value.length;
+        form = e.target.form,
+        incomplete = false,
+        digits = 0;
+
+    // update complete button availability
+    form.ans.forEach(n => {
+        digits += n.value.length;
         if (n.value === '') {
-            disabled = true;
+            incomplete = true;
         }
     });
-    if (len === 1 && !document.grid.dataset.chronoId) {
-        document.grid.dataset.chronoSecs = 0;
-        document.grid.dataset.chronoId = setInterval(function() {
-            document.grid.dataset.chronoSecs++;
+    form.complete.disabled = incomplete;
+
+    // start chrono on first digit entered
+    if (digits === 1 && !form.dataset.chronoId) {
+        form.dataset.chronoSecs = 0;
+        form.dataset.chronoId = setInterval(() => {
+            form.dataset.chronoSecs++;
         }, 1000);
     }
-    document.grid.complete.disabled = disabled;
 }
 
 function onCompleteClick(e) {
     e.preventDefault();
-    clearInterval(document.grid.dataset.chronoId);
-    delete document.grid.dataset.chronoId;
-    correct(document.grid);
+    clearInterval(e.target.form.dataset.chronoId);
+    delete e.target.form.dataset.chronoId;
+    correct(e.target.form);
 }
 
 
 /*
- * Operations
+ * 2. DOM manipulating
  */
 
 // Redraw grid operation nodes based on user ctrl input
@@ -97,86 +80,83 @@ function redraw(ctrl, grid) {
     var
         qty = parseInt(ctrl.elements.qty.value),
         nos = parseNumbers(ctrl.elements.nos.value),
-        xs  = weight(parseNumbers('0-10')),
+        weighted = weights(parseNumbers('0-10')),
         i,
         node,
-        ab,
-        prevAb = '',
+        ab, // [a, b]
+        prevAb = [],
         opr,
         ops = grid.querySelectorAll('.op'),
-        op = ops[0];
+        op = document.getElementById('grid-op').content.firstElementChild;
 
-    op.style.opacity = 0;
-    grid.complete.disabled = true;
-    grid.score.innerHTML = '';
-    grid.timer.innerHTML = '';
+    clearInterval(grid.dataset.chronoId);
+    delete grid.dataset.chronoId;
+
     grid.dataset.currentSticker = pick(grid.dataset.stickers.split(' '));
     grid.dataset.currentAnimation = pick(grid.dataset.animations.split(' '));
-
-    ops.forEach(grid.removeChild.bind(grid));
+    grid.innerHTML = '';
 
     for (i = 0; i < qty; i++) {
         // pick two numbers, different from previous selection
         do {
-            ab =  [pick(xs), pick(nos)];
-        } while (ab.toString() === prevAb.toString());
-        prevAb = ab.toString();
-        // pick a random
+            ab =  [pick(weighted), pick(nos)];
+        } while (ab.every(v => prevAb.includes(v)));
+        prevAb = ab;
+        // pick a random operator
         opr = pick(ctrl.querySelectorAll('[name="opr"]:checked')).value;
         // adjust ab based on operation
         if (opr === '÷') {
-            // make `xs` the answer
+            // make `weighted` the answer
             ab[0] *= ab[1];
         } else if (opr === '-') {
             // largest number first to never go negative
-            ab.sort(function (a, b) {
-                return b - a;
-            });
-        } else {
-            shuffle(ab);
+            ab.sort((a, b) => b - a);
         }
-        node = op.cloneNode(true);
-        node.querySelector('.o').innerHTML = opr;
-        node.querySelector('.a').innerHTML = ab[0];
-        node.querySelector('.b').innerHTML = ab[1];
+        let node = op.cloneNode(true);
+        node.querySelector('.o').innerText = opr;
+        node.querySelector('.a').innerText = ab[0];
+        node.querySelector('.b').innerText = ab[1];
         node.querySelector('input').value = '';
         node.classList.remove('err');
-        grid.insertBefore(node, grid.lastElementChild);
-        (function(node) {
-            // fade in tumble effect
-            setTimeout(function() {
-                node.style.opacity = 1;
-            }, 100 * i);
-        })(node);
+        grid.appendChild(node);
+
+        // fade in tumble effect
+        setTimeout(() => {
+            node.style.opacity = 1;
+        }, 100 * i);
     }
+    grid.appendChild(document.getElementById('grid-footer').content.cloneNode(true));
+    setTimeout(() => {
+        grid.lastElementChild.style.opacity = 1;
+    }, 100 * i);
 }
 
 // Mark invalid operations and show score
 function correct(grid) {
     var
         ops = grid.querySelectorAll('.op'),
-        input,
+        erroredInput,
         points = 0;
 
-    ops.forEach(function(op) {
+    ops.forEach(op => {
         var
-            a = parseInt(op.querySelector('.a').innerHTML),
-            b = parseInt(op.querySelector('.b').innerHTML),
+            a = parseInt(op.querySelector('.a').innerText),
+            b = parseInt(op.querySelector('.b').innerText),
             ans = parseInt(op.querySelector('input').value),
-            opr = op.querySelector('.o').innerHTML;
+            opr = op.querySelector('.o').innerText;
         if (calc(a, opr, b) !== ans) {
             op.classList.add('err');
             op.querySelector('input').value = '';
         } else {
-            points++;
             op.classList.remove('err');
+            points++;
         }
     });
-    if (input = grid.querySelector('.op.err input')) {
-        input.focus();
+    if (erroredInput = grid.querySelector('.op.err input')) {
+        erroredInput.focus();
     }
     grid.score.innerHTML = points + '/' + ops.length;
-    grid.timer.innerHTML = mmss(grid.dataset.chronoSecs);
+    grid.timer.innerText = mmss(grid.dataset.chronoSecs);
     if (points === ops.length) {
         grid.score.innerHTML += '<br><span class="animated ' + grid.dataset.currentAnimation + ' emojis emojis--' + grid.dataset.currentSticker + '"></span>';
     }
@@ -184,22 +164,24 @@ function correct(grid) {
 
 
 /*
- * Utilities
+ * 3. Purish functions
  */
 
 // Parse a `s` comma separated list of numbers
 // and ranges to an array of integers.
 function parseNumbers(s) {
-    result = [];
-    s.split(',').forEach(function(x) {
-        if (m = x.match(/(\d+)\s*\-\s*(\d+)/)) {
-            for (i = parseInt(m[1]); i <= m[2]; i++) {
+    var result = [];
+    s.split(',').forEach(x => {
+        let m = x.match(/(\d+)\s*\-\s*(\d+)/);
+        if (m) {
+            for (let i = parseInt(m[1]); i <= m[2]; i++) {
                 result.push(i);
             }
         } else {
             result.push(parseInt(x));
         }
     });
+    // return without duplicates
     return result.filter(indexAt);
 }
 
@@ -219,11 +201,9 @@ function calc(a, opr, b) {
     }
 }
 
-// Weight out easy numbers so they dont appear so often
-function weight(nos) {
-    return nos.reduce(function (acc, value, index, array) {
-        return acc.concat([0,1,10].includes(value) ? [value] : [value, value, value, value]);
-    }, []);
+// Weight out easy numbers so they appear 1/4
+function weights(nos) {
+    return nos.flatMap(v => new Array([0,1,10].includes(v) ? 1 : 4).fill(v));
 }
 
 // Pick a random value out of an array
@@ -235,7 +215,7 @@ function pick(a) {
 function shuffle(a) {
   var currentIndex = a.length, temporaryValue, randomIndex;
 
-  while (0 !== currentIndex) {
+  while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
     temporaryValue = a[currentIndex];
